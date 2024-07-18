87a68a602f64988f55ab4a938be33651428e6568
Grandis SYF <gra@dr.com>
2024-07-18 22:46:29 +0700

    update

---
+++ nvim/.config/nvim/lua/plugins/setup-whichkey.lua
@@ -0,0 +1,9 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  require("which-key").setup {
+    preset = "classic",
+    icons = {
+      rules = false,
+    },
+  },
+}
