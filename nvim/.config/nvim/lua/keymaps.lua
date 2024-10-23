9c7fedb54b6d017dba1b7ea03c50672feeb2022f
Grandis SYF <gra@dr.com>
2024-10-23 15:52:31 +0700

    update

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,9 +2,6 @@
 -- vim.api.nvim_set_keymap("n", "<C-j>", ":lua require('kulala').jump_next()<CR>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "<C-l>", ":lua require('kulala').run()<CR>", { noremap = true, silent = true })
 --
-vim.keymap.set("n", "<leader>gg", function()
-  print "Keymap triggered"
-  require("utils.lazygit").open_lazygit()
-end, {
-  desc = "Open Lazygit (tmux if available, otherwise in terminal)",
+vim.keymap.set("n", "<leader>gg", function() require("utils.lazygit").open_lazygit() end, {
+  desc = "LazyGit",
 })
