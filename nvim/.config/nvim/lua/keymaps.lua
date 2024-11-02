dbd37a883a4aa4996dba0d1327c042d15975bcf5
Grandis SYF <gra@dr.com>
2024-11-02 17:47:03 +0700

    update

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -9,3 +9,42 @@ vim.keymap.set("n", "<leader>gg", function() require("utils.lazygit").open_lazyg
 vim.keymap.set("n", "<leader>ta", function() require("utils.atac").open_atac() end, {
   desc = "Atac",
 })
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>as",
+  "<cmd>CodeCompanionActions<cr>",
+  { noremap = true, silent = true, desc = "AI Actions" }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<leader>as",
+  "<cmd>CodeCompanionActions<cr>",
+  { noremap = true, silent = true, desc = "AI Actions" }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>aa",
+  "<cmd>CodeCompanionChat Toggle<cr>",
+  { noremap = true, silent = true, desc = "AI Chat" }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<leader>aa",
+  "<cmd>CodeCompanionChat Toggle<cr>",
+  { noremap = true, silent = true, desc = "AI Chat" }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<leader>ad",
+  "<cmd>CodeCompanionChat Add<cr>",
+  { noremap = true, silent = true, desc = "AI Add" }
+)
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd [[cab cc CodeCompanion]]
+
+local wk = require "which-key"
+wk.add {
+  { "<leader>a", group = "󰚩 AI" },
+}
