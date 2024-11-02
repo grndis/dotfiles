a138b4dc8664da7368421d7f5a2a4f57332dcf3a
Grandis SYF <gra@dr.com>
2024-11-02 22:11:22 +0700

    update

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -20,6 +20,7 @@ return {
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.git.neogit" },
   -- { import = "astrocommunity.pack.vue" },
   -- { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.blade" },
