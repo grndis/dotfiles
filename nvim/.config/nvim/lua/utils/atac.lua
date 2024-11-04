c55405a7de96fd5a261dbaec6b33cf8598b388eb
Grandis SYF <gra@dr.com>
2024-11-04 14:02:21 +0700

    update

--- nvim/.config/nvim/lua/utils/atac.lua
+++ nvim/.config/nvim/lua/utils/atac.lua
@@ -1,8 +1,25 @@
+-- local M = {}
+--
+-- local fterm = require "FTerm"
+--
+-- M.atac = fterm:new {
+--   ft = "fterm_atac",
+--   cmd = "ATAC_THEME=$HOME/.config/atac/themes/theme.toml "
+--     .. "ATAC_KEY_BINDINGS=$HOME/.config/atac/key_bindings/vim.toml "
+--     .. "atac",
+--   dimensions = {
+--     height = 0.9,
+--     width = 0.9,
+--   },
+-- }
+--
+-- return M
+
 local M = {}
 
+-- Initialize FTerm lazygit instance
 local fterm = require "FTerm"
-
-M.atac = fterm:new {
+local lazygit_term = fterm:new {
   ft = "fterm_atac",
   cmd = "ATAC_THEME=$HOME/.config/atac/themes/theme.toml "
     .. "ATAC_KEY_BINDINGS=$HOME/.config/atac/key_bindings/vim.toml "
@@ -12,5 +29,17 @@ M.atac = fterm:new {
     width = 0.9,
   },
 }
+
+function M.open_atac()
+  local function is_tmux() return vim.env.TMUX ~= nil end
+
+  if is_tmux() then
+    -- Use tmux popup if in tmux
+    vim.fn.system 'tmux popup -d "#{pane_current_path}" -xC -yC -w90% -h90% -E "ATAC_THEME=$HOME/.config/atac/themes/theme.toml ATAC_KEY_BINDINGS=$HOME/.config/atac/key_bindings/vim.toml atac"'
+  else
+    -- Use FTerm if not in tmux
+    lazygit_term:toggle()
+  end
+end
 
 return M
