4a5e373f0a8e4850d76f958250c7642a98dfb14e
Grandis SYF <gra@dr.com>
2025-06-23 10:10:37 +0700

    feat: update lazy-lock.json and improve lazygit integration
    - Add lazygit.nvim and llm.nvim to lazy-lock.json
    
    - Uncomment lazygit keymap setup in keymaps.lua
    
    - Restore copilot integration in setup-blink.lua
    
    - Update lazygit and llm setup files for activation

--- nvim/.config/nvim/lua/plugins/setup-blink.lua
+++ nvim/.config/nvim/lua/plugins/setup-blink.lua
@@ -65,8 +65,8 @@ return {
       },
     },
     sources = {
-      -- default = { "lsp", "path", "snippets", "buffer", "copilot" },
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      -- default = { "lsp", "path", "snippets", "buffer" },
       providers = {
         -- codeium = {
         --   name = "codeium",
@@ -100,22 +100,22 @@ return {
         --     return items
         --   end,
         -- },
-        -- copilot = {
-        --   name = "copilot",
-        --   module = "blink-cmp-copilot",
-        --   score_offset = -100,
-        --   async = true,
-        --   min_keyword_length = 0,
-        --   transform_items = function(_, items)
-        --     local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
-        --     local kind_idx = #CompletionItemKind + 1
-        --     CompletionItemKind[kind_idx] = "Copilot"
-        --     for _, item in ipairs(items) do
-        --       item.kind = kind_idx
-        --     end
-        --     return items
-        --   end,
-        -- },
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = -100,
+          async = true,
+          min_keyword_length = 0,
+          transform_items = function(_, items)
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+            local kind_idx = #CompletionItemKind + 1
+            CompletionItemKind[kind_idx] = "Copilot"
+            for _, item in ipairs(items) do
+              item.kind = kind_idx
+            end
+            return items
+          end,
+        },
       },
     },
     keymap = {
