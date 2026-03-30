10e97bb5c5751a2dc2bb0a83ccbc4bf363312646
Grandis SYF <gra@dr.com>
2026-03-31 05:08:11 +0700

    feat(zellij): switch to zellij-nav.nvim with explicit keybindings

--- nvim/.config/nvim/lua/plugins/setup-zellij-nav.lua
+++ nvim/.config/nvim/lua/plugins/setup-zellij-nav.lua
@@ -1,6 +1,18 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
-  "hiasr/vim-zellij-navigator.nvim",
-  config = function() require("vim-zellij-navigator").setup() end,
+  "swaits/zellij-nav.nvim",
+  lazy = true,
+  event = "VeryLazy",
+  keys = {
+    { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+    { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+    { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+    { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+  },
+  opts = {},
 }
+-- return {
+--   "hiasr/vim-zellij-navigator.nvim",
+--   config = function() require("vim-zellij-navigator").setup() end,
+-- }
